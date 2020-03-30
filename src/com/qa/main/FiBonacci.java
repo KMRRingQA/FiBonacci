9268b511a69a8f0d6c7d59e686a5b87bd38bc19c
Korbinian <kmr.ring@outlook.com>
2020-03-30 20:34:31 +0100

    a *slightly* improved version, now full array-based arithmetics.

--- src/com/qa/main/FiBonacci.java
+++ src/com/qa/main/FiBonacci.java
@@ -1,16 +1,72 @@
 package com.qa.main;
 
 public class FiBonacci {
-	public void fibonacci(long limit) {
-		long int1 = 0;
-		long int2 = 1;
-		for (long intSum = 0; intSum < limit; intSum = int1 + int2) {
-			if (intSum > limit) {
+
+	public int[] add(int[] int1, int[] int2) {
+		int CarryOver = 0;
+		int[] int3 = new int[int1.length];
+		for (int i = int2.length - 1; i >= 0; i--) {
+			if (int1[i] + int2[i] + CarryOver > 9) {
+				int3[i] = int1[i] + int2[i] + CarryOver - 10;
+				CarryOver = 1;
+			} else {
+				int3[i] = int1[i] + int2[i] + CarryOver;
+				CarryOver = 0;
+			}
+		}
+		if (CarryOver == 1) {
+			int3[0] = 1;
+		}
+		return int3;
+	}
+
+	public void fibonacci(String limit) {
+		int length = limit.length() + 1;
+		int[] int1 = new int[length];
+		int[] int2 = new int[length];
+		System.out.print("1");
+		int2[length - 1] = 1;
+		while (true) {
+			int[] int3 = this.add(int1, int2);
+			if (!this.smallerEqualLimit(int3, limit)) {
 				break;
 			}
-			System.out.print(intSum + "  ");
+			this.printIntArray(int3);
 			int1 = int2;
-			int2 = intSum;
+			int2 = int3;
 		}
+
+	}
+
+	public void printIntArray(int[] int3) {
+		System.out.print("\n");
+		boolean numberStarted = false;
+		for (int i = 0; i < int3.length; i++) {
+			if (numberStarted == false && int3[i] == 0) {
+			} else {
+				numberStarted = true;
+				System.out.print(int3[i]);
+			}
+		}
+	}
+
+	public boolean smallerEqualLimit(int[] tempSum, String limit) {
+		int[] limitString = this.stringToIntArray(limit);
+		for (int i = 0; i < tempSum.length; i++) {
+			if (tempSum[i] > limitString[i]) {
+				return false;
+			} else if (tempSum[i] < limitString[i]) {
+				return true;
+			}
+		}
+		return true;
+	}
+
+	public int[] stringToIntArray(String limit) {
+		int[] limitString = new int[limit.length() + 1];
+		for (int i = 1; i <= limit.length(); i++) {
+			limitString[i] = Integer.parseInt(limit.substring(i - 1, i));
+		}
+		return limitString;
 	}
 }
--- src/com/qa/main/Runner.java
+++ src/com/qa/main/Runner.java
@@ -4,7 +4,8 @@ public class Runner {
 
 	public static void main(String[] args) {
 		FiBonacci fiBonacci = new FiBonacci();
-		fiBonacci.fibonacci(252345234634563456L);
+		fiBonacci.fibonacci(
+				"40278817102283407038585813270091176624224846310456696876645445975772848265708967220435913205");
 	}
 
 }
